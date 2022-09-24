fd7b5fc78b0559593f645ddc5895c5d47aa69a24
Pasan Withanage <pasanwithanage530@gmail.com>
2022-09-25 02:12:17 +0530

    aa

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
+import { BsInstagram } from 'react-icons/bs';
+import { AiFillFacebook } from 'react-icons/ai';
+import { BsWhatsapp } from 'react-icons/bs';
+
 import logo from '../assests/logo/Logo.png'
 
 function Header(props) {
@@ -43,6 +47,16 @@ function Header(props) {
           </Nav>
 
         </Navbar.Collapse>
+
+        {/* <LinkContainer to="/">
+          <Navbar.Brand><a href="https://www.facebook.com/bugsbunny23456" class="d-block" target="_blank"><AiFillFacebook/></a></Navbar.Brand>
+        </LinkContainer>
+        <LinkContainer to="/">
+          <Navbar.Brand><a href="//wa.me/+94711308121" class="d-block" target="_blank"><BsWhatsapp/></a></Navbar.Brand>
+        </LinkContainer>
+        <LinkContainer to="/">
+          <Navbar.Brand><a href="https://www.instagram.com/sadew_sanjula_anuhas/" class="d-block" target="_blank"><BsInstagram/></a></Navbar.Brand>
+        </LinkContainer> */}
         
       
       </Container>
