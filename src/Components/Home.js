60a3101a9c079c477f62cbed7fdca56ec1bcf5f4
Pasan Withanage <pasanwithanage530@gmail.com>
2022-09-25 01:44:33 +0530

    Home edits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,17 @@ import FloatingWhatsApp from 'react-floating-whatsapp'
 import avatar from '../assests/avatar.jpeg'
 
 import cover from '../assests/yala-natinal-park-sri-lanka-scaled.jpg';
-import cover2 from '../assests/ella-4-sri-lanka (1).jpg'
+import cover1 from '../assests/hummanaya-sri-lanka (1).jpg';
+import cover2 from '../assests/Kima_Surf_Home_Seminyak_Surf_Camp_2 (1).jpg'
+
+import h1 from '../assests/home/1.jpeg'
+import h2 from '../assests/home/2.jpeg'
+import h3 from '../assests/home/3.jpeg'
+import h4 from '../assests/home/4.jpeg'
+import h5 from '../assests/home/5.jpeg'
+import h6 from '../assests/home/6.jpeg'
+import h7 from '../assests/home/7.jpeg'
+import h8 from '../assests/home/8.jpeg'
 
 import { Link } from "react-router-dom";
 
@@ -86,6 +96,174 @@ function Home() {
 </div>
 </div>
 </section>
+
+
+
+
+<section class="gallery-area pt-100 pb-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h1} alt="Gallery Image" />
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h7} alt="Gallery Image"/>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h2} alt="Gallery Image" />
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h8} alt="Gallery Image" />
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h6} alt="Gallery Image"/>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="single-gallery-item mb-30">
+                            <img src={h4} alt="Gallery Image" />
+                        </div>
+                    </div>
+
+                    
+
+                    
+                    
+                    
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+<section class="about-area pt-100">
+<div class="container-fluid">
+<div class="row align-items-center">
+    
+
+
+
+    <div class="col-lg-6 col-md-12">
+        <div class="about-content left-content">
+            
+         
+
+            <h4>Hummanaya</h4><br/>
+            <p>Despite being a relatively small island, Sri Lanka is endowed with a diverse collection of landscapes, 
+                    climates and natural features. Sri Lanka’s infamous beaches have been enticing visitors for many years. However, the rest of 
+                    the island harbours many more idyllic vistas.
+
+                    The central highlands are carpeted with emerald tea plantations interspersed with occasional waterfalls. 
+                    Cool climates, misty views, and quaint estate bungalows are a throwback to bygone eras.
+
+                    The cultural triangle in the centre of the island holds a certain magic, with ruins from several ancient kingdoms, majestic 
+                    tanks, hidden rock caves, and ancient sacred temples, revealing the fascinating and almost unbelievable ancient architecture, art, and engineering.
+
+                    The North of Sri Lanka is a vibrant concoction of essences, with a unique flavour and culture. Despite still bearing the scars 
+                    from the civil war, the north truly bears testament to the diversity and variation that comprises the island. Influences include Portuguese, 
+                    Dutch, Tamil, Muslim, and British characters. Chains of islands are scattered off the shoreline, with their own unique habitats, waiting to be explored.
+
+                    The east of the island is home to lush paddy fields, spectacular sunrises, and a quieter more rural pace of life. Colourful places of worship, 
+                    national parks, ports and harbours, and pristine clear waters offer a mellow experience to any weary traveller.
+            </p>
+                            
+            
+        </div>
+
+        
+        
+    </div>
+   
+    <div class="col-lg-6 col-md-12">
+        <div class="about-right-imag">
+            <img src={cover1} className="home-image2" alt="image"/>
+            
+
+            {/* <div class="text-box">
+                <div class="inner">
+                    Trusted By
+                    <span>75K+</span>
+                </div>
+            </div> */}
+
+            
+        </div>
+    </div>
+</div>
+</div>
+</section>
+
+
+<section class="about-area pt-100">
+<div class="container-fluid">
+<div class="row align-items-center">
+    
+
+
+
+    <div class="col-lg-6 col-md-12">
+        <div class="about-content left-content">
+            
+         
+
+            <h4>The Kima Surf yoga & fitness resort Hiriketiya</h4><br/>
+            <p>The Kima Surf yoga & fitness resort Hiriketiya is conveniently positioned in a lush, green tropical environment, your new secure and comfortable eco-luxury home away from home. 
+                Chilling will come natural in the natural jungle surroundings whilst you 
+                guiltlessly indulge yourself in Kima’s newest eco-friendly resort, our surf camp in Hiriketiya.
+                The camp is situated near the famous Hiriketiya Bay and one of the most stunning surf beaches on Sri Lanka’s south coast. The waves are perfect for beginners to intermediates. 
+                More advanced, uncrowded waves are 
+                merely a brief Tuk Tuk ride away.Surfing is at the heart of Kima and our program, team and top-of-the-range equipment are unparalleled. Our add-on fitness programs compliment the
+                 surfing and our breezy training center overlooking the pool is the 
+                perfect space to learn and practice those important surf specific stretches.
+            </p>
+                            
+            
+        </div>
+
+        
+        
+    </div>
+   
+    <div class="col-lg-6 col-md-12">
+        <div class="about-right-imag">
+            <img src={cover2} className="home-image2" alt="image"/>
+            
+
+            {/* <div class="text-box">
+                <div class="inner">
+                    Trusted By
+                    <span>75K+</span>
+                </div>
+            </div> */}
+
+            
+        </div>
+    </div>
+</div>
+</div>
+</section>
+
+
+
+
+
 
 
 
